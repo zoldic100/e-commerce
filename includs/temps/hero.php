2af17abe70be1acb5f1a88757cb4fb574da75f24
zoldic100 <omarmaarouf86@gmail.com>
2023-05-05 11:38:32 +0100

    add hero area

---
+++ includs/temps/hero.php
@@ -0,0 +1,23 @@
+   <div class="container-fluid ">
+      <div class="hero">
+
+         <div class="left ">
+            <h1 class="text-start">   Find Everything You Need    <br>In One Place </h1>
+            <p>Our website is your one-stop shop for all your shopping needs. <br>
+               Shop and sell now and discover the endless possibilities
+            </p>
+            <div class="btns">
+               <button class="btn btn-light rounded-pill">shop now</button>
+               <button class="btn btn-light rounded-pill">sell now</button>
+            </div>
+         </div>
+         <div class="right">
+         
+
+
+         </div>
+      </div>
+
+      </div>
+   </div>
+</div>
--- init.php
+++ init.php
@@ -34,5 +34,5 @@ if(!isset($noNavBar)){
 include($temps."/navbar.php");
  }
 
-include($temps."/landing.php");
+include($temps."/hero.php");
  
